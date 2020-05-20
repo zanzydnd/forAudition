77725e78c0217408ae3f77298ef7e9538a575a65
zanzydnd <65661852+zanzydnd@users.noreply.github.com>
2020-05-20 18:59:31 +0300

    про меня

---
+++ Information.java
@@ -0,0 +1,10 @@
+package ru.kpfu.itis.group905.kozlov.sideProjects;
+
+public class Information {
+    public static void main(String[] args) {
+        String s = "Здравствуйте, меня зовут Даниил, я из 905 группы. Немного обо мне:" + "\n" +
+                "Раньше занимался олимпиадным программированием и математикой , потом ушел в музыку." + "\n"+
+                "Но не отпустили меня эти занятия до конца , поэтому я тут. Ну как-то так...";
+        System.out.println(s);
+    }
+}
